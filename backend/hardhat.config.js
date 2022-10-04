f6d87d33b28cb7b75218e8429dd46193de3b667b
Benjamin Hilder <benhilder7@gmail.com>
2022-10-04 17:11:39 +1030

    can retrieve proposal information on dapp

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 //contract address 0xD3150Eeb704a6caEAaBF812335ceb3804B76618A
 //0x6e083659a485f2ecc0b740a5b6e1913881bce65b
 //0x71ee163df5fc2c93333a8880b671ab2aa190ed6a
+//0x059F1b685D297ef48D2e1deB6a3090F132358B24
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,8 @@
 let provider = new ethers.providers.Web3Provider(window.ethereum)
 let signer
 
-const MultisigWalletAddress = "0x71ee163df5fc2c93333a8880b671ab2aa190ed6a"
+
+const MultisigWalletAddress = "0x059F1b685D297ef48D2e1deB6a3090F132358B24"
 const MultisigABI = ["function deposit() public payable",
                      "function getAllApprovers() public view returns(address[] memory)",
                      "function getProposalTitle(uint256 _id) public view returns(string memory)",
@@ -59,9 +60,53 @@ async function getAllApprovers() {
     document.getElementById("allApproversText").innerHTML = txResponse
 }
 
-async function getProposalInfo() {
-    const contract = new ethers.Contract(MultisigWalletAddress, MultisigABI, provider);
-    const txResponse = await contract.getProposalInfo(document.getElementById("infoInput").value)
+async function getInfo() {
+    getProposalTitle();
+    getProposalDescription();
+    getProposalEthAmount();
+    getProposalReciever();
+    getProposalApprovals();
+    getProposalDisapprovals();
+    getProposalIsActive();
+    getProposalFinishedResult();
+}
 
-    console.log("response: " + txResponse);
+async function getProposalTitle() {
+    const contract = new ethers.Contract(MultisigWalletAddress, MultisigABI, provider);
+    document.getElementById("proposalTitle").innerHTML = "Title: " + await contract.getProposalTitle(document.getElementById("infoInput").value)
+}
+
+async function getProposalDescription() {
+    const contract = new ethers.Contract(MultisigWalletAddress, MultisigABI, provider);
+    document.getElementById("proposalDescription").innerHTML = "Description: " + await contract.getProposalDescription(document.getElementById("infoInput").value)
+}
+
+async function getProposalEthAmount() {
+    const contract = new ethers.Contract(MultisigWalletAddress, MultisigABI, provider);
+    document.getElementById("proposalEthAmount").innerHTML = "Eth Amount: " + await contract.getProposalEthAmount(document.getElementById("infoInput").value)
+}
+
+async function getProposalReciever() {
+    const contract = new ethers.Contract(MultisigWalletAddress, MultisigABI, provider);
+    document.getElementById("proposalReciever").innerHTML = "Reciever: " + await contract.getProposalReciever(document.getElementById("infoInput").value)
+}
+
+async function getProposalApprovals() {
+    const contract = new ethers.Contract(MultisigWalletAddress, MultisigABI, provider);
+    document.getElementById("proposalApprovals").innerHTML = "Approvals: " + await contract.getProposalApprovals(document.getElementById("infoInput").value)
+}
+
+async function getProposalDisapprovals() {
+    const contract = new ethers.Contract(MultisigWalletAddress, MultisigABI, provider);
+    document.getElementById("proposalDisapprovals").innerHTML = "Disapprovals: " + await contract.getProposalDisapprovals(document.getElementById("infoInput").value)
+}
+
+async function getProposalIsActive() {
+    const contract = new ethers.Contract(MultisigWalletAddress, MultisigABI, provider);
+    document.getElementById("proposalIsActive").innerHTML = "Is Proposal Active? " + await contract.getProposalIsActive(document.getElementById("infoInput").value)
+}
+
+async function getProposalFinishedResult() {
+    const contract = new ethers.Contract(MultisigWalletAddress, MultisigABI, provider);
+    document.getElementById("proposalFinishedResult").innerHTML = "finished Result: " + await contract.getProposalFinishedResult(document.getElementById("infoInput").value)
 }
